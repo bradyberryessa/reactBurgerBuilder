d14dfb7c54455fb46a23f389e865cdc621831f5f
Brady Berryessa <brady.berryessa@gmail.com>
2018-12-29 23:34:11 -0700

    Added Burger and burger ingredient files with the css as well

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import classes from './Layout.module.css';
+
 const layout = props => (
 	<>
-		<div>Toolbar, SideDrawer, Backdrop</div>
-		<main>
+		<div className="Content">Toolbar, SideDrawer, Backdrop</div>
+		<main className={classes.Content}>
 			{props.children}
 		</main>
 	</>
