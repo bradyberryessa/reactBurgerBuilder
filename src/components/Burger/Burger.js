f91e2052fbb3ffa2e932cf8b9efb8090806ac106
Brady Berryessa <brady.berryessa@gmail.com>
2018-12-30 00:13:14 -0700

    Added alert to add ingredients if none are selected

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,14 +4,18 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = props => {
-	console.log(Object.keys(props.ingredients));
-	const ingredientsArray = Object.keys(props.ingredients)
+	let ingredientsArray = Object.keys(props.ingredients)
 		.map(ingredientKey => {
 			return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
 				return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
 			});
-		});
-
+		})
+		.reduce((array, element) => {
+			return array.concat(element)
+		}, []);
+	if (ingredientsArray.length === 0) {
+		ingredientsArray = <p>Please start adding ingredients!</p>;
+	}
 	return (
 		<div className={classes.Burger}>
 			<BurgerIngredient type="bread-top" />
