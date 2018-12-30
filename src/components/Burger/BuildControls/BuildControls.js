b47099ef6e49a601a2e1c73a78d6a46489572a97
Brady Berryessa <brady.berryessa@gmail.com>
2018-12-30 00:45:27 -0700

    can add ingredients in build control buy clicking on 'more' button

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -14,7 +14,11 @@ const buildControls = props => (
 	<div className={classes.BuildControls}>
 		{
 			controls.map(control => {
-				return <BuildControl label={control.label} />
+				return <BuildControl
+					key={control.label}
+					label={control.label}
+					added={() => props.ingredientAdded(control.type)}
+				/>
 			})
 		}
 	</div>
