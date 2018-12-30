9121abf867489381f68de22729eed132b920ef7f
Brady Berryessa <brady.berryessa@gmail.com>
2018-12-29 23:14:29 -0700

    started building the layout for the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
 	render() {
 		return (
 			<div>
-				App
+				<Layout>
+					<p>Testing</p>
+				</Layout>
 			</div>
 		);
 	}
