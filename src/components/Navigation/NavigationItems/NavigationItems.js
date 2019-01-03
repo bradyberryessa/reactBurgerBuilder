c3930ee592518930bcdf5551470f2b233b3348c1
Brady Berryessa <brady.berryessa@gmail.com>
2019-01-02 22:13:15 -0700

    Created an order and orders components and hooked them up

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -3,11 +3,11 @@ import React from 'react';
 import NavigationItem from './NavigationItem/NavigationItem';
 import classes from './NavigationItems.module.css';
 
-const navigationItems = props => {
+const navigationItems = () => {
 	return (
 		<ul className={classes.NavigationItems}>
 			<NavigationItem link="/">Burger Builder</NavigationItem>
-			<NavigationItem link="/checkout">Checkout</NavigationItem>
+			<NavigationItem link="/orders">Orders</NavigationItem>
 		</ul>
 	)
 };
